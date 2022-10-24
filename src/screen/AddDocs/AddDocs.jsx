e9f878f90645a8ffb6b078c59957d3d64d289c54
JANARTHANAN-T <janathangavel0911@gmail.com>
2022-10-24 14:37:42 +0530

    partial complete

--- src/screen/AddDocs/AddDocs.jsx
+++ src/screen/AddDocs/AddDocs.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './AddDocs.css'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 function AddDocs() {
+  const navigate = useNavigate();
+  const [name,setName]=useState('');
+  const [description, setDescription]=useState('');
+
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    await axios({
+      method: 'post',
+      url: `http://localhost:4000/doc/${user._id}/adddocs`,
+      data: {name,description}
+    }).then((res)=>{
+      console.log(res);
+      localStorage.setItem("document",JSON.stringify(res.data.document))
+      navigate("/")
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+  
+
   return (
     <div className='container my-5'>
               <div className='row d-flex align-items-center'>
@@ -13,15 +37,15 @@ function AddDocs() {
             <div class="card-body p-5">
         <form>
             <div class="mb-3">
-                <label for="title" class="form-label">Document Title</label>
-                <input type="text" class="form-control" id="title" name='title' aria-describedby="emailHelp" />
+                <label HtmlFor="title" class="form-label">Document Title</label>
+                <input type="text" class="form-control" id="title" name='title' value={name} onChange={(e)=>setName(e.target.value)} aria-describedby="emailHelp" />
             </div>
             <div class="mb-3">
-                <label for="description" class="form-label">Document Description</label>
-                <input type="password" class="form-control" id="description" name='description' />
+                <label HtmlFor="description" class="form-label">Document Description</label>
+                <input type="text" class="form-control" id="description" name='description' value={description} onChange={(e)=>setDescription(e.target.value)} />
             </div>
             <div className='text-center'>
-            <button type="submit" class="btn btn-success" >Create</button>
+            <button type="submit" class="btn btn-success" onClick={handleSubmit} >Create</button>
             </div>
         </form>
         </div>
--- src/screen/Home/Home.jsx
+++ src/screen/Home/Home.jsx
@@ -1,120 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const [documents, setDocument] = useState();
+  const navigate = useNavigate();
+  // let documents
+  useEffect(() => {
+    //  documents=JSON.parse(localStorage.getItem("document"))
+    setDocument(JSON.parse(localStorage.getItem("document")));
+  }, [documents]);
+  const user = localStorage.getItem("user");
+  const handleLogOut = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("document");
+    navigate("/auth");
+  };
+
   return (
     <div className="container my-5">
       <div className="mx-5 d-flex justify-content-end">
         <div className="col-8 px-3 ">
-        <div class="input-group mb-3">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Search Docs"
-            aria-label="Recipient's username"
-            aria-describedby="button-addon2"
-          />
-          <button
-            class="btn btn-outline-secondary"
-            type="button"
-            id="button-addon2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-search"
-              viewBox="0 0 16 16"
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Search Docs"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <button
+              class="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
             >
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-            </svg>
-          </button>
-        </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
+            </button>
+          </div>
         </div>
         <div className="col-2">
-        <Link to="/adddocs/" className="link">
-        <button className="btn btn-success">Add Docs</button>
-        </Link>
+          <Link to="/adddocs/" className="link">
+            <button className="btn btn-success">Add Docs</button>
+          </Link>
         </div>
         <div className="col-2">
-        <Link to="/auth/" className="link">
-          LOGIN/REGISTER
-        </Link>
+          {user ? (
+            <div onClick={handleLogOut}>LOGOUT</div>
+          ) : (
+            <Link to="/auth/" className="link">
+              LOGIN/REGISTER
+            </Link>
+          )}
         </div>
       </div>
-
-      <div className="row">
-        <div className="col-md-4 p-5">
-          <Link to="/docs/1" className="link">
-            <div class="card shadow home-card">
-              <div class="card-body">
-                <h3 className="heading">Software Engineering</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Veritatis, soluta in quidem error provident eius qui ducimus
-                  molestias.
-                </p>
+      {documents ? (
+        <div className="row">
+          {documents.map((ele, index) => {
+            const link=`/docs/${ele._id}`
+            return (
+              <div className="col-md-4 p-5 " key={index}>
+                <Link to={link} className="link">
+                  <div class="card shadow home-card">
+                    <div class="card-body">
+                      <h3 className="heading">{ele.name}</h3>
+                      <p>{ele.description}</p>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </div>
-          </Link>
+            );
+          })}
         </div>
-        <div className="col-md-4 p-5">
-          <Link to="/docs/1" className="link">
-            <div class="card shadow home-card">
-              <div class="card-body">
-                <h3 className="heading">Computer Networks</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Veritatis, soluta in quidem error provident eius qui ducimus
-                  molestias.
-                </p>
-              </div>
-            </div>
-          </Link>
+      ) : (
+        <div className="d-flex justify-content-center align-items-center mt-5 pt-5">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>
-        <div className="col-md-4 p-5">
-          <Link to="/docs/1" className="link">
-            <div class="card shadow home-card">
-              <div class="card-body p-4">
-                <h3 className="heading">Energy Conservation</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Veritatis, soluta in quidem error provident eius qui ducimus.
-                </p>
-              </div>
-            </div>
-          </Link>
         </div>
-        <div className="col-md-4 p-5">
-          <Link to="/docs/1" className="link">
-            <div class="card shadow home-card">
-              <div class="card-body">
-                <h3 className="heading">Operating System</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Veritatis, soluta in quidem error provident eius qui ducimus
-                  molestias.
-                </p>
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className="col-md-4 p-5">
-          <Link to="/docs/1" className="link">
-            <div class="card shadow home-card">
-              <div class="card-body">
-                <h3 className="heading">User Design And Interface</h3>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Veritatis, soluta in quidem error provident eius qui.
-                </p>
-              </div>
-            </div>
-          </Link>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
